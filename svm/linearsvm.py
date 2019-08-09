4790e68b8d68235fa3ff77a3c0905c342cc53d8e
徐炜 <xuwei@xuweideMacBook-Pro.local>
2019-08-09 22:23:45 +0800

    add linear svm deep-version

---
+++ svm/linearsvm.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tensorflow as tf
+
+
+class LinearSVM(object):
+    def __init__(self,
+                 learning_rate=0.01,
+                 max_iteration=2000):
+        self.learning_rate = learning_rate
+        self.max_iteration = max_iteration
+        self.sess = tf.InteractiveSession()
+        self.x = None
+        self.y = None
+        self.y_ = None
+        self.w = None
+        self.b = None
+
+    def fit(self, x_train, y_train, batch_size=32):
+        x_train = np.array(x_train)
+        y_train = np.array(y_train).reshape(-1, 1)
+        print(x_train.shape)
+        print(y_train.shape)
+        self.x = tf.placeholder(shape=[None, x_train.shape[1]], dtype=tf.float64, name='input_data')
+        self.y_ = tf.placeholder(shape=[None, 1], dtype=tf.float64, name='label')
+        self.w = tf.Variable(tf.random_normal(shape=[x_train.shape[1], 1], dtype=tf.float64))
+        self.b = tf.Variable(tf.constant(value=0.01, shape=[1], dtype=tf.float64))
+        self.y = tf.matmul(self.x, self.w) + self.b
+        loss = tf.reduce_sum(tf.math.maximum(tf.cast(0,tf.float64), 1 - self.y * self.y_))
+        # loss = -tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.y, labels=self.y_))
+        train = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+        self.sess.run(tf.global_variables_initializer())
+
+        for i in range(self.max_iteration):
+            rnd_indices = np.random.randint(0, len(x_train), batch_size)
+            x_batch = x_train[rnd_indices]
+            y_batch = y_train[rnd_indices]
+            self.sess.run(train, feed_dict={self.x: x_batch, self.y_: y_batch})
+            if i % 100 == 0:
+                obj = self.sess.run(loss, feed_dict={self.x: x_train,
+                                                     self.y_: y_train})
+                print(obj)
+
+    def predict(self, x_test):
+        y_predict = self.sess.run(self.y, feed_dict={self.x: x_test})
+        y_predict[y_predict >= 0] = 1
+        y_predict[y_predict < 0] = -1
+
+        return y_predict
+
+
+if __name__ == '__main__':
+    from sklearn.datasets import load_digits
+    from sklearn.model_selection import train_test_split
+
+    # from sklearn.linear_model import LogisticRegression
+
+    raw_data = load_digits(n_class=2)
+    X = np.array(raw_data.data)
+    y = np.array(raw_data.target)
+    for i in range(len(y)):
+        if y[i] == 0:
+            y[i] = -1
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    clf = LinearSVM(max_iteration=100)
+    clf.fit(np.array(X_train), np.array(y_train))
+    # print(X_test.shape)
+    y_ = clf.predict(np.array(X_test))
+    count = 0
+    for i in range(len(y_)):
+        if y_test[i] == y_[i]:
+            count += 1
+
+    print(count / len(y_))
